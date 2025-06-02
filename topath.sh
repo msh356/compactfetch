3b9499d7042ac8961bec748dd2200b79da959644
msh356 <msh356@yandex.by>
2025-06-02 12:09:31 +0300

    make file executable

--- topath.sh
+++ topath.sh
@@ -3,6 +3,7 @@ if [ $EUID -ne 0 ]; then
 	echo "[!] Please, run as root"
 	exit 1
 fi
+chmod +x main.py
 cp main.py /usr/local/bin/compactfetch
 echo "[*] Now you can run compactfetch from terminal, just"
 echo "[*] entering compactfetch in terminal. Restart your"
